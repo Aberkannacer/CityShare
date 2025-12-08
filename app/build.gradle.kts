08cdae271b2c1e7cfe5b61e36c85808eec4d8c51
Nacer <aberkannacer04@gmail.com>
2025-12-08 20:31:20 +0100

    Fix errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.services)
-    id("org.jetbrains.kotlin.kapt")
+    id("kotlin-kapt")
 }
 
 android {
